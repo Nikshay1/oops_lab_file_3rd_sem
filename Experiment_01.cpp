a0338b36b7d96cf83a49460eda303bf279ce5b0f
Nikshay1 <142086467+Nikshay1@users.noreply.github.com>
2024-10-20 22:38:39 +0530

    Update Experiment_01.cpp

--- Experiment_01.cpp
+++ Experiment_01.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+class PersonalDetails {
+private:
     string name;
     char surname_initial;
     int total_marks;
     char gender;
     char result;
 
-    cout << "Enter your name: ";
-    cin >> name;
+public:
+    void getData() {
+        cout << "Enter your name: ";
+        getline(cin, name);
+        cout << "Enter your surname initial: ";
+        cin >> surname_initial;
+        cout << "Enter your total marks: ";
+        cin >> total_marks;
+        cout << "Enter your gender (M/F): ";
+        cin >> gender;
+        cout << "Enter your result (P/F): ";
+        cin >> result;
+    }
+    void displayData() {
+        cout << "\n--- Personal Details ---" << endl;
+        cout << "Name: " << name << endl;
+        cout << "Surname Initial: " << surname_initial << endl;
+        cout << "Total Marks: " << total_marks << endl;
+        cout << "Gender: " << gender << endl;
+        cout << "Result: " << result << endl;
+    }
+};
 
-    cout << "Enter your surname initial: ";
-    cin >> surname_initial;
+int main() {
+    PersonalDetails person;
 
-    cout << "Enter your total marks: ";
-    cin >> total_marks;
-
-    cout << "Enter your gender (M/F): ";
-    cin >> gender;
-
-    cout << "Enter your result (P/F): ";
-    cin >> result;
-
-    cout << "\n--- Personal Details ---" << endl;
-    cout << "Name: " << name << endl;
-    cout << "Surname initial: " << surname_initial << endl;
-    cout << "Total Marks: " << total_marks << endl;
-    cout << "Gender: " << gender << endl;
-    cout << "Result: " << result << endl;
+    person.getData();
+  
+    person.displayData();
 
     return 0;
 }
